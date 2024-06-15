9b8da4ab6b54aecc9e77e7256c851358619b722f
VRomanenko04 <vovasan2004@gmail.com>
2024-06-15 21:11:04 +0300

    Made a Header for the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import styles from "@/styles/mainPage.module.scss";
+import Image from "next/image";
+import Logo_wave from '@/assets/dispersion_wave.svg';
+import Link from "next/link";
+
 
 export default function MainPage() {
   return (
-    <main className={styles.main}>
-
-    </main>
+    <>
+    <header className={styles.header}>
+      <Link href='/' className={styles.logo}>
+        <Image src={Logo_wave} alt="Dispersion" className={styles.image}/>
+        <h1>dispersion</h1>
+      </Link>
+    </header>
+    </>
   );
 }
