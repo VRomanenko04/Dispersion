114ac480cc25592487f0f66b5c24514a1a8f9366
VRomanenko04 <vovasan2004@gmail.com>
2024-06-16 01:56:37 +0300

    Wrapped the logo in a container and added a new component to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,23 @@ import styles from "@/styles/mainPage.module.scss";
 import Image from "next/image";
 import Logo_wave from '@/assets/dispersion_wave.svg';
 import Link from "next/link";
+import OurSpecialty from "@/components/OurSpecialty/OurSpecialty";
 
 
 export default function MainPage() {
   return (
     <>
     <header className={styles.header}>
-      <Link href='/' className={styles.logo}>
-        <Image src={Logo_wave} alt="Dispersion" className={styles.image}/>
-        <h1>dispersion</h1>
-      </Link>
+      <div>
+        <Link href='/' className={styles.logo}>
+          <Image src={Logo_wave} alt="Dispersion" className={styles.image}/>
+          <h1>dispersion</h1>
+        </Link>
+      </div>
     </header>
+    <main>
+      <OurSpecialty />
+    </main>
     </>
   );
 }
