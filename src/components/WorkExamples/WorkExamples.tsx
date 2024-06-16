b8cc2e5f783306c0f9df67277e91f3002b66a5c7
VRomanenko04 <vovasan2004@gmail.com>
2024-06-17 02:01:29 +0300

    Wrote a skeleton component for an example of work and animation for it

---
+++ src/components/WorkExamples/WorkExamples.tsx
@@ -0,0 +1,58 @@
+'use client'
+import React, { useRef } from 'react';
+import styles from './workExamples.module.scss';
+import { motion, useScroll, useTransform } from 'framer-motion';
+
+const WorkExamples = () => {
+    const ref = useRef<HTMLDivElement>(null);
+    const { scrollYProgress } = useScroll({
+        target: ref,
+    });
+
+    const moveDown = useTransform(scrollYProgress, [0, 1], [150, 0]);
+    const moveUp = useTransform(scrollYProgress, [0, 1], [-150, 0]);
+
+    return (
+        <section className={styles.wrapper}>
+            <div className={styles.container} ref={ref}>
+                <motion.div 
+                    className={styles.flex__start}
+                    style={{ y: moveDown }}
+                >
+                    <div className={styles.img__sceleton}></div>
+                    <div className={styles.img__sceleton}></div>
+                </motion.div>
+                <motion.div 
+                    className={styles.flex__end}
+                    style={{ y: moveUp }}
+                >
+                    <div className={styles.img__sceleton}></div>
+                    <div className={styles.img__sceleton}></div>
+                </motion.div>
+                <motion.div 
+                    className={styles.flex__start}
+                    style={{ y: moveDown }}
+                >
+                    <div className={styles.img__sceleton}></div>
+                    <div className={styles.img__sceleton}></div>
+                </motion.div>
+                <motion.div 
+                    className={styles.flex__end}
+                    style={{ y: moveUp }}
+                >
+                    <div className={styles.img__sceleton}></div>
+                    <div className={styles.img__sceleton}></div>
+                </motion.div>
+                <motion.div 
+                    className={styles.flex__start}
+                    style={{ y: moveDown }}
+                >
+                    <div className={styles.img__sceleton}></div>
+                    <div className={styles.img__sceleton}></div>
+                </motion.div>
+            </div>
+        </section>
+    )
+}
+
+export default WorkExamples;
