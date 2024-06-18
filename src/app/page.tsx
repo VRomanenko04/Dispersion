597155ead456a00f3d49f2787832e3d1431d37c0
VRomanenko04 <vovasan2004@gmail.com>
2024-06-18 21:39:30 +0300

    Added a navigation block to the main page, set the font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import Logo_wave from '@/assets/dispersion_wave.svg';
 import Link from "next/link";
 import OurSpecialty from "@/components/OurSpecialty/OurSpecialty";
 import WorkExamples from "@/components/WorkExamples/WorkExamples";
+import Navigation from "@/components/Navigation/Navigation";
+import { Be_Vietnam_Pro } from "next/font/google";
 
+const beVietnamPro = Be_Vietnam_Pro({subsets: ["latin"], weight: ["400", "500", "600", "700"] });
 
 export default function MainPage() {
   return (
     <>
-    <header className={styles.header}>
+    <header className={`${styles.header} ${beVietnamPro.className}`}>
       <div>
         <Link href='/' className={styles.logo}>
           <Image src={Logo_wave} alt="Dispersion" className={styles.image}/>
@@ -17,9 +20,10 @@ export default function MainPage() {
         </Link>
       </div>
     </header>
-    <main>
+    <main className={beVietnamPro.className}>
       <OurSpecialty />
       <WorkExamples />
+      <Navigation />
     </main>
     </>
   );
