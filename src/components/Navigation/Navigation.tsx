3d3b82dcf2a3b375f332b24e101343bb8eb09070
VRomanenko04 <vovasan2004@gmail.com>
2024-06-20 18:44:21 +0300

    Added animation of page change in navbar

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -9,9 +9,24 @@ import Portfolio_3d from '@/assets/bag_3d.png';
 
 const Navigation = () => {
 const [navImage, setNavImage] = useState("Website creation");
+const [changePage, setChangePage] = useState("Website creation");
+const [isHidden, setIsHidden] = useState(false);
 
 const chosenImage = navImage === "Website creation" ? Web_3d : navImage === "Design services" ? Design_3d : navImage === "Portfolio" ? Portfolio_3d : '';
-const chosenBlur = `${navImage === "Website creation" ? styles.blue__blur : navImage === "Design services" ? styles.pink__blur : navImage === "Portfolio" ? styles.purple_blur : ''}`
+const chosenBlur = `${styles.blur} ${navImage === "Website creation" ? styles.blue__blur : navImage === "Design services" ? styles.pink__blur : navImage === "Portfolio" ? styles.purple_blur : ''}`;
+
+const handleNavClick = (imageType: string) => {
+    if (navImage !== imageType) {
+        setChangePage(imageType);
+        setIsHidden(true);
+        setTimeout(() => {
+            setIsHidden(false);
+        }, 800);
+        setTimeout(() => {
+            setNavImage(imageType);
+        }, 600);
+    }
+};
 
     return (
         <section className={styles.wrapper}>
@@ -19,31 +34,31 @@ const chosenBlur = `${navImage === "Website creation" ? styles.blue__blur : navI
                 <nav>
                     <ul className={styles.menu}>
                         <li 
-                            className={`${styles.menu__item} ${navImage === "Website creation" && styles.active__first}`} 
-                            onClick={() => setNavImage("Website creation")}
+                            className={`${styles.menu__item} ${changePage === "Website creation" && styles.active__first}`} 
+                            onClick={() => handleNavClick("Website creation")}
                         >
                             <div className={styles.line}></div>
                             <h3>Website creation</h3>
                         </li>
                         <li 
-                            className={`${styles.menu__item} ${navImage === "Design services" && styles.active__second}`} 
-                            onClick={() => setNavImage("Design services")}
+                            className={`${styles.menu__item} ${changePage === "Design services" && styles.active__second}`} 
+                            onClick={() => handleNavClick("Design services")}
                         >
                             <div className={styles.line}></div>
                             <h3>Design services</h3>
                         </li>
                         <li 
-                            className={`${styles.menu__item} ${navImage === "Portfolio" && styles.active__third}`} 
-                            onClick={() => setNavImage("Portfolio")}
+                            className={`${styles.menu__item} ${changePage === "Portfolio" && styles.active__third}`} 
+                            onClick={() => handleNavClick("Portfolio")}
                         >
                             <div className={styles.line}></div>
                             <h3>Portfolio</h3>
                         </li>
                     </ul>
                 </nav>
-                <div>
-                    <Image width={900} height={900} src={chosenImage} alt={navImage}/>
-                    <div className={chosenBlur}></div>
+                <div className={styles.image__block}>
+                    <Image className={`${styles.image} ${isHidden ? styles.hidden__image : ''}`} src={chosenImage} alt={navImage}/>
+                    <div className={`${chosenBlur} ${isHidden ? styles.hidden : ''}`}></div>
                 </div>
             </div>
         </section>
