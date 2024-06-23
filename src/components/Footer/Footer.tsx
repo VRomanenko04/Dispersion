5276ecd2376af6b59ac2c8225cba1f105b9e38cc
VRomanenko04 <vovasan2004@gmail.com>
2024-06-24 02:03:50 +0300

    Wrote a footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './Footer.module.scss';
+import Logo from '@/assets/dispersion_logo.svg';
+import Link from 'next/link';
+
+type FooterProps = {
+    color: string
+}
+
+const Footer = ({ color }: FooterProps) => {
+    return (
+        <footer className={`${styles.footer} ${color === 'blue' ? styles.blue : color === 'magenta' ? styles.magenta : color === 'purple' ? styles.purple : ''}`}>
+            <div className={styles.footer__container}>
+                <address className={styles.adress}>
+                    <h6>Support:</h6>
+                    <p>Inst: <a className={styles.link} href="https://www.instagram.com">@dispertion_</a></p>
+                    <p>E-mail: <a className={styles.link} href="mailto: dispertion@gmail.com">dispertion@gmail.com</a></p>
+                </address>
+                <Link href='/'>
+                    <Image src={Logo} alt='Dispersion logo' className={styles.logo}/>
+                </Link>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
