2fa573db17af6722229e496b9723a6ab46f58135
VRomanenko04 <vovasan2004@gmail.com>
2024-07-15 03:44:52 +0300

    Added a form to the contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import styles from './ContactPage.module.scss';
 import Image from 'next/image';
 import Logo from '@/assets/dispersion_logo.svg';
+import ContactForm from '@/components/ContactForm/ContactForm';
+import { Be_Vietnam_Pro } from 'next/font/google';
+import Link from 'next/link';
 
+const beVietnamPro = Be_Vietnam_Pro({subsets: ["latin"], weight: ["400", "500", "600", "700"] });
 
 const ContactPage = () => {
     return (
         <>
-            <header className={styles.header__wrapper}>
+            <header className={`${styles.header__wrapper} ${beVietnamPro.className}`}>
                 <div className={styles.header__container}>
-                    <Image src={Logo} alt='Dispersion logo' className={styles.logo}/>
+                    <Link href='/'>
+                        <Image src={Logo} alt='Dispersion logo' className={styles.logo}/>
+                    </Link>
                 </div>
             </header>
-            <main>
-                {/* Form component */}
+            <main className={`${styles.main__wrapper} ${beVietnamPro.className}`}>
+                <ContactForm />
             </main>
         </>
     )
