9995b5430b72b0ae4c3570c72375a613e4665480
VRomanenko04 <vovasan2004@gmail.com>
2024-07-30 20:10:51 +0300

    Wrote an api function to send requests by email

---
+++ src/app/api/sendMail/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.SENDER_MAIL,
+        pass: process.env.SENDER_SECRET_CODE,
+    }
+});
+
+interface MailData {
+    fullName: string;
+    email: string;
+    projectType: string;
+    howToContact: string;
+    contactDetails?: string;
+    message: string;
+};
+
+export async function POST(req: NextRequest) {
+    try {
+        // Explicitly parse the request body as JSON
+        const requestData: MailData = await req.json();
+
+        const contactDetailsString = requestData.contactDetails ? `Информация для связи: ${requestData.contactDetails}` : '';
+
+        // Content of the email
+        const mailOptions = {
+            from: process.env.SENDER_MAIL,
+            to: process.env.MAIL_TO_RECEIVING,
+            subject: 'Новый запрос',
+            text: `Имя: ${requestData.fullName}\nEmail: ${requestData.email}\nКак связаться: ${requestData.howToContact}\n${contactDetailsString}\nТип проекта: ${requestData.projectType}\nОписание: ${requestData.message}`,
+        };
+
+        await transporter.sendMail(mailOptions);
+        return NextResponse.json({ success: true });
+        } catch (error) {
+            console.error(error);
+            return NextResponse.json({ success: false, error: 'Ошибка при отправке письма' });
+        }
+}
