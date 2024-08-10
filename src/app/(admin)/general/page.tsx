88a20f2447ee5d20df565c748da05da80f52d02d
VRomanenko04 <vovasan2004@gmail.com>
2024-08-10 19:18:59 +0300

    Created files for the main admin page

---
+++ src/app/(admin)/general/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './GeneralPanel.module.scss';
+
+
+const GeneralPanel = () => {
+    return (
+        <div>Page</div>
+    )
+}
+
+export default GeneralPanel;
