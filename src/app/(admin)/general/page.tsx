311544ada675bce31a57ad6f0803fe964c8de887
VRomanenko04 <vovasan2004@gmail.com>
2024-08-14 17:44:14 +0300

    Started general page admin panel

--- src/app/(admin)/general/page.tsx
+++ src/app/(admin)/general/page.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styles from './GeneralPanel.module.scss';
+import ActiveOrdersBlock from '@/components/admin/ActiveOrdersBlock/ActiveOrdersBlock';
 
 
 const GeneralPanel = () => {
     return (
-        <div>Page</div>
+        <div className={styles.page__wrapper}>
+            <section>
+                <ActiveOrdersBlock />
+                {/* Statistic */}
+            </section>
+            <section>
+                {/* General to-do list */}
+            </section>
+        </div>
     )
 }
 
