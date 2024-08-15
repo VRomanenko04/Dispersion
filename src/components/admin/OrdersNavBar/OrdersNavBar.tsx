6ecbc731140f9a745b979ed098e928432c19f89a
VRomanenko04 <vovasan2004@gmail.com>
2024-08-15 16:38:48 +0300

    Wrote a separate Navbar for order pages

---
+++ src/components/admin/OrdersNavBar/OrdersNavBar.tsx
@@ -0,0 +1,41 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+import styles from './OrdersNavBar.module.scss';
+import Link from 'next/link';
+import { GetProjectsCount } from '@/services/GetProjectsCount';
+import { usePathname } from 'next/navigation';
+
+const OrdersNavBar = () => {
+    const [projectsCount, setProjectsCount] = useState({
+        under_consideration: 0,
+        active: 0,
+        declined: 0,
+    });
+
+    useEffect(() => {
+        const fetchProjectsCount = async () => {
+            try {
+                const result = await GetProjectsCount();
+                setProjectsCount(result);
+            } catch (error) {
+                console.error('Error fetching projects count:', error);
+            }
+        };
+
+        fetchProjectsCount();
+    }, []);
+
+    const currentPath = usePathname();
+    
+    return (
+        <nav className={styles.container}>
+            <ul className={styles.list}>
+                <Link className={`${styles.link} ${currentPath === '/orders/consideration' ? styles.active : ''}`} href='/orders/consideration'>На расмотрении <p>({projectsCount.under_consideration})</p></Link>
+                <Link className={`${styles.link} ${currentPath === '/orders/active' ? styles.active : ''}`} href='/orders/active'>Принятые заявки <p>({projectsCount.active})</p></Link>
+                <Link className={`${styles.link} ${currentPath === '/orders/declined' ? styles.active : ''}`} href='/orders/declined'>Отклонённые заявки <p>({projectsCount.declined})</p></Link>
+            </ul>
+        </nav>
+    )
+}
+
+export default OrdersNavBar;
