56fbcde19926d5b9645c96759db32398c665199b
VRomanenko04 <vovasan2004@gmail.com>
2024-08-17 17:11:31 +0300

    Inserted a modal window into projects under review

--- src/components/admin/UnderConsiderationProject/UnderConsiderationProject.tsx
+++ src/components/admin/UnderConsiderationProject/UnderConsiderationProject.tsx
@@ -1,9 +1,10 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './UnderConsiderationProject.module.scss';
 import Image from 'next/image';
 import checkmark from '@/assets/check_mark_icon.svg';
 import dicline from '@/assets/dicline_icon.svg';
+import DeleteModalWindow from '../DeleteModalWindow/DeleteModalWindow';
 
 type UnderConsiderationProjectProps = {
     orderCode: string
@@ -15,24 +16,29 @@ type UnderConsiderationProjectProps = {
 }
 
 const UnderConsiderationProject = (props: UnderConsiderationProjectProps) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
-        <div className={styles.container}>
-            <div className={styles.prev__img}>
-                <div></div>
+        <>
+            <div className={styles.container}>
+                <div className={styles.prev__img}>
+                    <div></div>
+                </div>
+                <h6 className={styles.title}>{props.fullName}</h6>
+                <p className={styles.order__code}>{props.orderCode}</p>
+                <div className={styles.buttons__container}>
+                    <button className={styles.accept}>
+                        <Image src={checkmark} alt='Checkmark icon'/>
+                        Принять
+                    </button>
+                    <button className={styles.dicline} onClick={() => setIsModalOpen(prev => !prev)}>
+                        <Image src={dicline} alt='Dicline icon'/>
+                        Отклонить
+                    </button>
+                </div>
             </div>
-            <h6 className={styles.title}>{props.fullName}</h6>
-            <p className={styles.order__code}>{props.orderCode}</p>
-            <div className={styles.buttons__container}>
-                <button className={styles.accept}>
-                    <Image src={checkmark} alt='Checkmark icon'/>
-                    Принять
-                </button>
-                <button className={styles.dicline}>
-                    <Image src={dicline} alt='Dicline icon'/>
-                    Отклонить
-                </button>
-            </div>
-        </div>
+            <DeleteModalWindow setIsOpen={setIsModalOpen} isOpen={isModalOpen}/>
+        </>
     )
 }
 
