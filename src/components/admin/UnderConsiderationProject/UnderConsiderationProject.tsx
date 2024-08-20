4b229538b41fda056008dfa91ed73006c9bd5b03
VRomanenko04 <vovasan2004@gmail.com>
2024-08-21 01:10:33 +0300

    Added a block of additional information about the project, made an animation

--- src/components/admin/UnderConsiderationProject/UnderConsiderationProject.tsx
+++ src/components/admin/UnderConsiderationProject/UnderConsiderationProject.tsx
@@ -8,12 +8,14 @@ import DeleteModalWindow from '../DeleteModalWindow/DeleteModalWindow';
 import { OrderRejection } from '@/services/OrderRejection';
 import AcceptProjectModalWindow from '../AcceptProjectModalWindow/AcceptProjectModalWindow';
 import { AcceptOrder } from '@/services/AcceptOrder';
+import { AnimatePresence, motion } from 'framer-motion';
 
 type UnderConsiderationProjectProps = {
     orderCode: string
     fullName: string
     email: string
     additionalContact?: string
+    howToContact: string
     message: string
     projectType: string
 }
@@ -21,6 +23,7 @@ type UnderConsiderationProjectProps = {
 const UnderConsiderationProject = (props: UnderConsiderationProjectProps) => {
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
     const [isAcceptModalOpen, setIsAcceptModalOpen] = useState(false);
+    const [isProjectOpen, setIsProjectOpen] = useState(false);
 
 
     const handleDeclineOrder = async () => {
@@ -37,25 +40,56 @@ const UnderConsiderationProject = (props: UnderConsiderationProjectProps) => {
         })
     }
 
+    const containerStyles = `${!isProjectOpen ? styles.container : styles.open__container}`
+
     return (
         <>
-            <div className={styles.container}>
-                <div className={styles.prev__img}>
-                    <div></div>
+            <section className={styles.wrapper}>
+                <div className={containerStyles} onClick={() => setIsProjectOpen(prev => !prev)}>
+                    <div className={styles.prev__img}>
+                        <div></div>
+                    </div>
+                    <h6 className={styles.title}>{props.fullName}</h6>
+                    <p className={styles.order__code}>{isProjectOpen && 'Код заказа: '}№ {props.orderCode}</p>
+                    <div className={styles.buttons__container}>
+                        <button className={styles.accept} onClick={() => setIsAcceptModalOpen(true)}>
+                            <Image src={checkmark} alt='Checkmark icon'/>
+                            Принять
+                        </button>
+                        <button className={styles.dicline} onClick={() => setIsDeleteModalOpen(true)}>
+                            <Image src={dicline} alt='Dicline icon'/>
+                            Отклонить
+                        </button>
+                    </div>
                 </div>
-                <h6 className={styles.title}>{props.fullName}</h6>
-                <p className={styles.order__code}>№ {props.orderCode}</p>
-                <div className={styles.buttons__container}>
-                    <button className={styles.accept} onClick={() => setIsAcceptModalOpen(true)}>
-                        <Image src={checkmark} alt='Checkmark icon'/>
-                        Принять
-                    </button>
-                    <button className={styles.dicline} onClick={() => setIsDeleteModalOpen(true)}>
-                        <Image src={dicline} alt='Dicline icon'/>
-                        Отклонить
-                    </button>
-                </div>
-            </div>
+                <AnimatePresence>
+                    {isProjectOpen && (
+                        <motion.div 
+                            className={styles.additional__container}
+                            initial={{ height: 0 }}
+                            animate={{ height: 'auto' }}
+                            exit={{ height: 0 }}
+                            transition={{ duration: 0.2 }}
+                        >
+                            <div className={styles.contact__container}>
+                                <p className={styles.sub__title}>Контактная информация:</p>
+                                <div className={styles.contacts}>
+                                    <p>Full Name: <b>{props.fullName}</b></p>
+                                    <p>Email: <b>{props.email}</b></p>
+                                    <p>Contact type: <b>{props.howToContact}</b></p>
+                                    {props.howToContact !== 'Email' && props.additionalContact && (
+                                        <p>Phone number or @: <b>{props.additionalContact}</b></p>
+                                    )}
+                                </div>
+                            </div>
+                            <div className={styles.message__container}>
+                                <p className={styles.sub__title}>Описание работы:</p>
+                                <p className={styles.message}>{props.message}</p>
+                            </div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </section>
             <DeleteModalWindow setIsOpen={setIsDeleteModalOpen} isOpen={isDeleteModalOpen} deleteFunction={handleDeclineOrder}/>
             <AcceptProjectModalWindow setIsOpen={setIsAcceptModalOpen} isOpen={isAcceptModalOpen} acceptFunction={handleAcceptOrder}/>
         </>
