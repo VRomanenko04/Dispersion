be6809cec5200bb50095600c56a36b517db4e957
VRomanenko04 <vovasan2004@gmail.com>
2024-08-21 15:16:29 +0300

    Made the functionality of logging out of the account on the header

--- src/components/admin/Header/Header.tsx
+++ src/components/admin/Header/Header.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
 import styles from './Header.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
 import Logo from '@/assets/dispersion_logo.svg';
+import Logout_icon from '@/assets/Logout_icon.svg';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@/app/firebase';
+import { AdminLogout } from '@/services/AdminLogout';
 
 const Header = () => {
+    const [email, setEmail] = useState<string | null>(null);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setEmail(user.email);
+            } else {
+                setEmail(null)
+            }
+        })
+    }, [])
+
+    const handleLogOut = async () => {
+        await AdminLogout();
+    }
+
     return (
         <header className={styles.header__wrapper}>
             <div className={styles.header__container}>
                 <Link href='/general'>
                     <Image src={Logo} alt='Dispersion logo' className={styles.logo}/>
                 </Link>
+                <div className={styles.logout__container}>
+                    <p>{email}</p>
+                    <Image onClick={handleLogOut} className={styles.logout__icon} src={Logout_icon} alt='Logout icon'/>
+                </div>
             </div>
         </header>
     )
