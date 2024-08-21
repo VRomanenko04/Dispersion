d903294076514fb05800dccd7dc5514f1819c58a
VRomanenko04 <vovasan2004@gmail.com>
2024-08-21 20:48:03 +0300

    Temporarily added a search component to the page

--- src/app/(admin)/orders/consideration/page.tsx
+++ src/app/(admin)/orders/consideration/page.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import styles from './UnderConsideration.module.scss';
 import UnderConsiderationBlock from '@/components/admin/UnderConsiderationBlock/UnderConsiderationBlock';
 import ProtectedPage from '@/components/admin/ProtectedPage';
+import SearchInput from '@/components/admin/SearchInput/SearchInput';
 
 
 const UnderConsiderationPage = () => {
     return (
         <section className={styles.container}>
+            <SearchInput />
             <UnderConsiderationBlock />
         </section>
     )
