8f501a2a212d74023f8108ac7333b570b56bedd0
VRomanenko04 <vovasan2004@gmail.com>
2024-08-23 15:07:53 +0300

    Completely externally made the active order component

---
+++ src/components/admin/AcceptedProject/AcceptedProject.tsx
@@ -0,0 +1,87 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import styles from './AcceptedProject.module.scss';
+import { AnimatePresence, motion } from 'framer-motion';
+
+type AcceptedProjectProps = {
+    email: string
+    fullName: string
+    howToContact: string
+    message: string
+    orderCode: string
+    contactDetails?: string
+    projectName: string
+    deadline: string
+}
+
+const AcceptedProject = (props: AcceptedProjectProps) => {
+    const [isProjectOpen, setIsProjectOpen] = useState(false);
+    const [timeToDeadline, setTimeToDeadline] = useState('');
+
+    useEffect(() => {
+        const deadlineDate = new Date(props.deadline);
+        const now = new Date();
+        const diff = deadlineDate.getTime() - now.getTime();
+
+        if (diff > 0) {
+            const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+            setTimeToDeadline(`${days}д. ${hours}ч.`);
+        } else {
+            setTimeToDeadline('Срок истек');
+        }
+    }, []);
+
+    const containerStyles = `${!isProjectOpen ? styles.container : styles.open__container}`
+
+    return (
+        <section className={styles.wrapper}>
+            <div className={containerStyles} onClick={() => setIsProjectOpen(prev => !prev)}>
+                <div className={styles.prev__img}>
+                    <div></div>
+                </div>
+                <h6 className={styles.title}>{props.projectName}</h6>
+                <p className={styles.order__code}>{isProjectOpen && 'Код заказа: '}№ {props.orderCode}</p>
+                <p className={styles.deadline}>{timeToDeadline}</p>
+                <p className={styles.status}>В процессе</p>
+            </div>
+            <AnimatePresence>
+                {isProjectOpen && (
+                    <motion.div
+                        className={styles.additional__container}
+                        initial={{ height: 0 }}
+                        animate={{ height: 'auto' }}
+                        exit={{ height: 0 }}
+                        transition={{ duration: 0.2 }}
+                    >
+                        {isProjectOpen && <motion.button 
+                                            initial={{ opacity: 0 }} 
+                                            animate={{ opacity: 1 }} 
+                                            exit={{ opacity: 0}} 
+                                            transition={{ duration: 0.1 }} 
+                                            className={styles.btn}>Завершить
+                                        </motion.button>}
+                        <div className={styles.contact__container}>
+                                <p className={styles.sub__title}>Контактная информация:</p>
+                                <div className={styles.contacts}>
+                                    <p>Full Name: <b>{props.fullName}</b></p>
+                                    <p>Email: <b>{props.email}</b></p>
+                                    <p>Contact type: <b>{props.howToContact}</b></p>
+                                    {props.howToContact !== 'Email' && props.contactDetails && (
+                                        <p>Phone number or @: <b>{props.contactDetails}</b></p>
+                                    )}
+                                </div>
+                            </div>
+                            <div className={styles.message__container}>
+                                <p className={styles.sub__title}>Описание работы:</p>
+                                <p className={styles.message}>{props.message}</p>
+                            </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </section>
+    )
+}
+
+export default AcceptedProject;
