f55c1248ce95adf23454d57b03ca5f3812c562d9
VRomanenko04 <vovasan2004@gmail.com>
2024-08-24 00:09:05 +0300

    Made a modal window to confirm the completion of the order

---
+++ src/components/admin/CompleteModalWindow/CompleteModalWindow.tsx
@@ -0,0 +1,24 @@
+'use client';
+import React from 'react';
+import styles from './CompleteModalWindow.module.scss';
+import UiPopUp from '../UiPopUp/UiPopUp';
+
+type CompleteModalWindowProps = {
+    isOpen: boolean
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+    completeFunction: () => void
+}
+
+const CompleteModalWindow = ({ isOpen, setIsOpen, completeFunction }: CompleteModalWindowProps) => {
+    return (
+        <UiPopUp isOpen={isOpen} setIsOpen={setIsOpen}>
+            <h6 className={styles.title}>Вы уверены?</h6>
+            <section className={styles.btns}>
+                <button className={styles.confirm__btn} onClick={completeFunction}>Да, завершить</button>
+                <button className={styles.dicline__btn} onClick={() => setIsOpen(false)}>Отмена</button>
+            </section>
+        </UiPopUp>
+    )
+}
+
+export default CompleteModalWindow;
