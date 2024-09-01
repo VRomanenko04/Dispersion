7533d6bc61498d0668c1eb4c867bd5504a76052b
VRomanenko04 <vovasan2004@gmail.com>
2024-09-01 23:18:28 +0300

    Finished the block of active orders for the main admin menu

--- src/components/admin/ActiveOrdersBlock/ActiveOrdersBlock.tsx
+++ src/components/admin/ActiveOrdersBlock/ActiveOrdersBlock.tsx
@@ -2,26 +2,31 @@
 import React, { useEffect, useState } from 'react';
 import styles from './ActiveOrdersBlock.module.scss';
 import { GetProjectsData } from '@/services/GetData';
+import Link from 'next/link';
+import Image from 'next/image';
+import ArrowIcon from '@/assets/ArrowIcon.svg';
+import ActiveOrder from '../ActiveOrder/ActiveOrder';
 
 type ProjectObject = {
-    email: string
-    fullName: string
-    howToContact: string
-    message: string
-    projectType: string
+    orderCode: string
+    projectName: string
+    status?: string
+    deadline: string
 }
 
-
 const ActiveOrdersBlock = () => {
-    const [underConsiderationProjects, setUnderConsiderationProjects] = useState<ProjectObject[]>([]);
+    const [activeOrders, setActiveOrders] = useState<ProjectObject[]>([]);
 
     const inizializeProjects = async () => {
-        const projectsList = await GetProjectsData('under_consideration');
+        const projectsList = await GetProjectsData('accepted');
 
         if (projectsList) {
             // Преобразование объекта в массив объектов
             const projectsArray = Object.values(projectsList) as ProjectObject[];
-            setUnderConsiderationProjects(projectsArray);
+
+            projectsArray.sort((a, b) => new Date(a.deadline).getTime() - new Date(b.deadline).getTime());
+            
+            setActiveOrders(projectsArray);
         }
     };
 
@@ -32,10 +37,41 @@ const ActiveOrdersBlock = () => {
 
 
     return (
-        <div>
-            {underConsiderationProjects.map((project, index) => (
-                <li key={index}>{project.fullName} {project.message}</li>
-            ))}
+        <div className={styles.container}>
+            <h6 className={styles.title}>Активные заказы</h6>
+            <section className={styles.project__container}>
+                <div className={styles.filter__container}>
+                    <div className={styles.prev}>
+                        <p>Превью</p>
+                    </div>
+                    <div className={styles.name}>
+                        <p>Название</p>
+                    </div>
+                    <div className={styles.order__code}>
+                        <p>№ Код заказа</p>
+                    </div>
+                    <div className={styles.time__left}>
+                        <p>Осталось времени:</p>
+                    </div>
+                </div>
+                <div className={styles.projects__container}>
+                    {activeOrders.map((project) => (
+                        <div key={project.orderCode}>
+                            <ActiveOrder 
+                                orderCode={project.orderCode}
+                                projectStatus={project.status}
+                                projectName={project.projectName}
+                                deadline={project.deadline}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </section>
+            <section>
+            </section>
+            <Link href='/orders/accepted' className={styles.link}>
+                <Image src={ArrowIcon} alt='Arrow link' />
+            </Link>
         </div>
     )
 }
