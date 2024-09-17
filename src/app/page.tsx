c7b69c6646ba22885d95c1f14b0e05714fbc70f7
VRomanenko04 <vovasan2004@gmail.com>
2024-09-17 17:24:20 +0300

    Added a site header to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,14 @@ import OurSpecialty from "@/components/user/OurSpecialty/OurSpecialty";
 import WorkExamples from "@/components/user/WorkExamples/WorkExamples";
 import { Be_Vietnam_Pro } from "next/font/google";
 import SubPages from "@/components/user/SubPages/SubPages";
+import HomeHead from "@/components/user/HomeHead/HomeHead";
 
 const beVietnamPro = Be_Vietnam_Pro({subsets: ["latin"], weight: ["400", "500", "600", "700"] });
 
 export default function MainPage() {
   return (
     <>
-    <header className={`${styles.header} ${beVietnamPro.className}`}>
-      <div>
-        <Link href='/' className={styles.logo}>
-          <Image src={Logo_wave} alt="Dispersion" className={styles.image}/>
-          <h1>dispersion</h1>
-        </Link>
-      </div>
-    </header>
+    <HomeHead />
     <main className={beVietnamPro.className}>
       <OurSpecialty />
       <WorkExamples />
