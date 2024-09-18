499af711fc3cc5d47f8fb59c206eecf6871a40f0
VRomanenko04 <vovasan2004@gmail.com>
2024-09-18 13:16:50 +0300

    Wrote a service for adding a task block

---
+++ src/services/AddTask.ts
@@ -0,0 +1,36 @@
+import { database } from "@/app/firebase"
+import { ref, set } from "firebase/database"
+import { GetProjectsData } from "./GetData";
+
+interface Project {
+    name: string;
+    orderCode: string;
+}
+
+export const AddTask = async (email: string, projectName: string) => {
+    try {
+        const sanitizedEmail = email.split('.')[0];
+
+        const newTaskRef = ref(database, `tasks/${sanitizedEmail}/${projectName}`);
+
+        const Projects: any = await GetProjectsData('accepted');
+
+        const foundProject = Projects.find((project: any) => project.projectName === projectName);
+
+        if (!foundProject) {
+            throw new Error(`Project with orderName: ${projectName} not found`);
+        }
+
+        const orderCode = foundProject.orderCode;
+
+        const TaskInfo = {
+            'orderCode': orderCode,
+        }
+
+        await set(newTaskRef, TaskInfo);
+
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
