5832d49cc125f9e2fc33c5b90154f6d095e27beb
VRomanenko04 <vovasan2004@gmail.com>
2024-09-18 13:16:24 +0300

    Prepared the todo page for work

---
+++ src/app/(admin)/todo/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react';
+import styles from './TodoPage.module.scss';
+
+
+const TodoPage = () => {
+
+    return (
+        <section className={styles.container}>
+            {/* <SearchBar />
+            <TodoBlock /> */}
+        </section>
+    )
+}
+
+export default TodoPage;
