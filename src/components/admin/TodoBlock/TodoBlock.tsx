432532e8d8e3064d1c20a107f013689857f6ed57
VRomanenko04 <vovasan2004@gmail.com>
2024-09-30 23:21:09 +0300

    Improved the component for deleting task lists

--- src/components/admin/TodoBlock/TodoBlock.tsx
+++ src/components/admin/TodoBlock/TodoBlock.tsx
@@ -7,11 +7,16 @@ import { GetTasks } from '@/services/GetTasks';
 import { TodoProjectObject } from '@/app/(admin)/todo/page';
 import SearchBar from '../SearchBar/SearchBar';
 import TodoPrototype from '../TodoPrototype/TodoPrototype';
+import { DeleteTaskList } from '@/services/DeleteTaskList';
 
 
 const TodoBlock = () => {
     const [todoList, setTodoList] = useState<TodoProjectObject[]>([]);
     const [filteredList, setFilteredList] = useState<TodoProjectObject[]>([]);
+    const [selectedTasks, setSelectedTasks] = useState<string[]>([]);
+
+    const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+
 
     const inizializeTasks = () => {
         onAuthStateChanged(auth, async (user) => {
@@ -42,17 +47,49 @@ const TodoBlock = () => {
         setFilteredList(filtered);
     };
 
+    const handleTasksSelection = (projectName: string) => {
+        setSelectedTasks((prevSelected) =>
+            prevSelected.includes(projectName)
+                ? prevSelected.filter((name) => name !== projectName)
+                : [...prevSelected, projectName]
+        );
+    };
+
+    const handleDeleteSelectedProjects = async () => {
+        onAuthStateChanged(auth, async (user) => {
+            if (user?.email) {
+                const userKey = user.email.split('.')[0];
+                await DeleteTaskList(userKey, selectedTasks);
+
+                setSelectedTasks([]);
+                setIsDeleteOpen(false);
+                inizializeTasks();
+            }
+        });
+    };
+
     useEffect(() => {
         inizializeTasks();
     }, []);
 
     return (
         <section className={styles.wrapper}>
-            <SearchBar onChangeFunc={handleSearchChange}/>
+            <SearchBar 
+                onChangeFunc={handleSearchChange} 
+                onDeleteProjects={handleDeleteSelectedProjects}
+                isOpen={isDeleteOpen}
+                setIsOpen={setIsDeleteOpen}
+                initialize={inizializeTasks}
+            />
             <section className={styles.container}>
                 {filteredList?.map((task) => (
                     <div key={task.orderCode}>
-                        <TodoPrototype orderCode={task.orderCode} projectName={task.projectName}/>
+                        <TodoPrototype 
+                            orderCode={task.orderCode} 
+                            projectName={task.projectName}
+                            isSelected={selectedTasks.includes(task.projectName)}
+                            onSelect={handleTasksSelection}
+                        />
                     </div>
                 ))}
             </section>
