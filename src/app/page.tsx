afa723170f5547635c43d82ca3291fc48b893773
VRomanenko04 <vovasan2004@gmail.com>
2024-10-05 17:35:04 +0300

    Created conditional rendering for different screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,42 @@
-import styles from "@/styles/mainPage.module.scss";
-import Image from "next/image";
-import Logo_wave from '@/assets/dispersion_wave.svg';
-import Link from "next/link";
+'use client';
 import OurSpecialty from "@/components/user/OurSpecialty/OurSpecialty";
 import WorkExamples from "@/components/user/WorkExamples/WorkExamples";
 import { Be_Vietnam_Pro } from "next/font/google";
 import SubPages from "@/components/user/SubPages/SubPages";
 import HomeHead from "@/components/user/HomeHead/HomeHead";
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import MobileWorkExamples from "@/components/user/MobileWorkExamples/MobileWorkExamples";
+import MiddleWorkExamples from "@/components/user/WorkExamples/MiddleWorkExamples";
 
 const beVietnamPro = Be_Vietnam_Pro({subsets: ["latin"], weight: ["400", "500", "600", "700"] });
 
 export default function MainPage() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  const isMobile = useMediaQuery({ query: '(max-width: 800px)' });
+  const isMiddleDevice = useMediaQuery({query: '(min-width: 800px) and (max-width: 1400px)'});
+  
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+}
+
   return (
     <>
     <HomeHead />
     <main className={beVietnamPro.className}>
       <OurSpecialty />
-      <WorkExamples />
+      {isMobile ? (
+          <MobileWorkExamples />
+        ) : isMiddleDevice ? (
+          <MiddleWorkExamples />
+        ) : (
+          <WorkExamples />
+        )}
       <SubPages />
     </main>
     </>
