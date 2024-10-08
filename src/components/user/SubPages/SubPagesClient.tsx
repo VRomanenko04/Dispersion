dd84389688d75721e46fc79002bf93bc51d8389c
VRomanenko04 <vovasan2004@gmail.com>
2024-10-08 08:17:16 +0300

    Reworked the subpages component

--- src/components/user/SubPages/SubPagesClient.tsx
+++ src/components/user/SubPages/SubPagesClient.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react';
-import { useMediaQuery } from '@uidotdev/usehooks';
+import { useMediaQuery } from 'react-responsive';
 
 import Navigation from '../Navigation/Navigation';
 import WebdevSubPage from '../WebdevSubPage/WebdevSubPage';
@@ -8,6 +8,7 @@ import DesignSubPage from '../DesignSubPage/DesignSubPage';
 import Footer from '../Footer/Footer';
 import PortfolioSubPage from '../PortfolioSubPage/PortfolioSubPage';
 import NavBar from '../NavBar/NavBar';
+import MobileNavBar from '../MobileNavBar/MobileNavBar';
 
 type SubPagesClientProps = {
     navVisible: boolean
@@ -16,11 +17,12 @@ type SubPagesClientProps = {
 }
 
 const SubPagesClient = ({ navVisible, navRef, setVisibleTrigger }: SubPagesClientProps) => {
-    const smallHeightDevice = useMediaQuery("only screen and (max-height : 720px)");
-    const mediumHeightDevice = useMediaQuery("only screen and (min-height : 721px) and (max-height : 900px)");
-    const largeHeightDevice = useMediaQuery("only screen and (min-height : 901px) and (max-height : 1200px)");
-    const ultraLargeHeightDevice = useMediaQuery("only screen and (min-height : 1201px) and (max-height : 1350px)");
-    const monsterLargeHeightDevice = useMediaQuery("only screen and (min-height : 1351px)");
+    const isMobile = useMediaQuery({query: "(max-width : 800px)"});
+    const smallHeightDevice = useMediaQuery({query:"only screen and (max-height : 720px)"});
+    const mediumHeightDevice = useMediaQuery({query:"only screen and (min-height : 721px) and (max-height : 900px)"});
+    const largeHeightDevice = useMediaQuery({query:"only screen and (min-height : 901px) and (max-height : 1200px)"});
+    const ultraLargeHeightDevice = useMediaQuery({query:"only screen and (min-height : 1201px) and (max-height : 1350px)"});
+    const monsterLargeHeightDevice = useMediaQuery({query:"only screen and (min-height : 1351px)"});
 
     const [currentPage, setCurrentPage] = useState("Website creation");
     const [navImage, setNavImage] = useState("Website creation");
@@ -34,21 +36,30 @@ const SubPagesClient = ({ navVisible, navRef, setVisibleTrigger }: SubPagesClien
 
     return (
         <>
-            <Navigation 
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
-                items={items}
-                setItems={setItems}
-                navImage={navImage}
-                setNavImage={setNavImage}
-            />
-            <NavBar 
-                setActiveLink={setCurrentPage} 
-                setOtherImage={setNavImage} 
-                setItemsIndex={setItems} 
-                activeLink={currentPage} 
-                navVisible={navVisible}
-            />
+            {!isMobile ? (
+                <>
+                    <Navigation 
+                        currentPage={currentPage}
+                        setCurrentPage={setCurrentPage}
+                        items={items}
+                        setItems={setItems}
+                        navImage={navImage}
+                        setNavImage={setNavImage}
+                    />
+                    <NavBar 
+                        setActiveLink={setCurrentPage} 
+                        setOtherImage={setNavImage} 
+                        setItemsIndex={setItems} 
+                        activeLink={currentPage} 
+                        navVisible={navVisible}
+                    />
+                </>
+            ) : <MobileNavBar 
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                    setOtherImage={setNavImage} 
+                    setItemsIndex={setItems} 
+                />}
             <section ref={navRef}>
                 {currentPage === "Website creation" && <WebdevSubPage isActive={currentPage === "Website creation"}/>}
                 {currentPage === "Design services" && <DesignSubPage isActive={currentPage === "Design services"}/>}
