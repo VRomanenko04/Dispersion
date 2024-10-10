64df83d1932e2623275694a0fbb0d1cd1596938c
VRomanenko04 <vovasan2004@gmail.com>
2024-10-10 16:45:59 +0300

    Adapted contacts helper

--- src/components/user/ContactsHelper/ContactsHelper.tsx
+++ src/components/user/ContactsHelper/ContactsHelper.tsx
@@ -1,28 +1,59 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './ContactsHelper.module.scss';
 import Image from 'next/image';
 import InformationIcon from '@/assets/Information_icon.svg';
 import { motion } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
 
 
 const ContactsHelper = () => {
     const [isHovered, setIsHovered] = useState(false);
+    const commentRef = useRef<HTMLDivElement>(null);
+
+    const isMobile = useMediaQuery({query: "(max-width : 800px)"});
+
+    const handleClick = () => {
+        setIsHovered(prev => !prev);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (commentRef.current && !commentRef.current.contains(event.target as Node)) {
+                setIsHovered(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return (
         <div className={styles.container}>
-            <Image 
-                className={styles.image} 
-                src={InformationIcon} 
-                alt='Information button'
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-            />
+            {isMobile ? (
+                <Image 
+                    className={styles.image} 
+                    src={InformationIcon} 
+                    alt='Information button'
+                    onClick={handleClick}
+                />
+            ) : (
+                <Image 
+                    className={styles.image} 
+                    src={InformationIcon} 
+                    alt='Information button'
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                />
+            )}
             <motion.div 
+                ref={commentRef}
                 className={styles.comment__container}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: isHovered ? 1 : 0 }}
-                transition={{ duration: 0.2, delay: 0.3 }}
+                transition={ !isMobile ? { duration: 0.2, delay: 0.3 } : { duration: 0.2 }}
             >
                 <div className={styles.triangle}></div>
                 <div className={styles.comment__block}>
