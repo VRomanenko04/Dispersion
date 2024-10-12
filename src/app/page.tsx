672b6f1bf073454925aee58e53591de108e33349
VRomanenko04 <vovasan2004@gmail.com>
2024-10-12 20:53:09 +0300

    Made the main page a server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,20 @@
-'use client';
 import OurSpecialty from "@/components/user/OurSpecialty/OurSpecialty";
-import WorkExamples from "@/components/user/WorkExamples/WorkExamples";
 import { Be_Vietnam_Pro } from "next/font/google";
 import SubPages from "@/components/user/SubPages/SubPages";
 import HomeHead from "@/components/user/HomeHead/HomeHead";
-import { useEffect, useState } from "react";
-import { useMediaQuery } from "react-responsive";
-import MobileWorkExamples from "@/components/user/MobileWorkExamples/MobileWorkExamples";
-import MiddleWorkExamples from "@/components/user/WorkExamples/MiddleWorkExamples";
+import WorkExamplesMedia from "@/components/user/WorkExamplesMedia";
 
 const beVietnamPro = Be_Vietnam_Pro({subsets: ["latin"], weight: ["400", "500", "600", "700"] });
 
 export default function MainPage() {
-  const [isMounted, setIsMounted] = useState(false);
-
-  const isMobile = useMediaQuery({ query: '(max-width: 800px)' });
-  const isMiddleDevice = useMediaQuery({query: '(min-width: 800px) and (max-width: 1400px)'});
-  
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    return null;
-}
-
   return (
     <>
-    <HomeHead />
-    <main className={beVietnamPro.className}>
-      <OurSpecialty />
-      {isMobile ? (
-          <MobileWorkExamples />
-        ) : isMiddleDevice ? (
-          <MiddleWorkExamples />
-        ) : (
-          <WorkExamples />
-        )}
-      <SubPages />
-    </main>
+      <HomeHead />
+      <main className={beVietnamPro.className}>
+        <OurSpecialty />
+        <WorkExamplesMedia />
+        <SubPages />
+      </main>
     </>
   );
 }
