9110ceacd3dafeb67dcbb507223aacc350f604fe
VRomanenko04 <vovasan2004@gmail.com>
2024-10-13 03:40:58 +0300

    Made a preliminary adaptation of the block of our specialty

--- src/components/user/OurSpecialty/OurSpecialty.tsx
+++ src/components/user/OurSpecialty/OurSpecialty.tsx
@@ -1,7 +1,9 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './ourSpecialty.module.scss';
-import { motion } from 'framer-motion';
+import { AnimatePresence, animate, motion, useMotionValue } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
+import useMeasure from 'react-use-measure';
 
 const emergenceVariant = {
     hidden: {
@@ -12,8 +14,54 @@ const emergenceVariant = {
     }
 }
 
+const firstList = ['Webdev', 'Optimization', 'Landing', 'SEO', 'Business', 'Logotype', 'Social', '3D Visualization', 'Animation'];
+const secondList = ['NextJS', 'TypeScript', 'Website', 'E-commerce', 'Individual', 'Advertizing', 'Branding', 'Stylization'];
+
 const OurSpecialty = () => {
     const [hasAnimated, setHasAnimated] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
+    const isMobile = useMediaQuery({ query: '(max-width: 800px)' });
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    let [refFirst, { width: widthFirst }] = useMeasure();
+    let [refSecond, { width: widthSecond }] = useMeasure();
+
+    const xTranslationFirst = useMotionValue(0);
+    const xTranslationSecond = useMotionValue(0);
+
+    useEffect(() => {
+        let controls;
+        let finalPosition = -widthFirst / 2 - 1600;
+
+        controls = animate(xTranslationFirst, [0, finalPosition], {
+            ease: 'linear',
+            duration: 25,
+            repeat: Infinity,
+            repeatType: 'loop',
+            repeatDelay: 0
+        })
+
+        return controls.stop;
+    }, [xTranslationFirst, widthFirst]);
+
+    useEffect(() => {
+        let controls;
+        let startPosition = -widthSecond * 2.2;
+        let finalPosition = widthSecond - widthSecond / 1.2;
+
+        controls = animate(xTranslationSecond, [startPosition, finalPosition], {
+            ease: 'linear',
+            duration: 20,
+            repeat: Infinity,
+            repeatType: 'loop',
+            repeatDelay: 0
+        })
+
+        return controls.stop;
+    }, [xTranslationSecond, widthSecond]);
 
     const handleViewportEnter = () => {
         if (!hasAnimated) {
@@ -21,32 +69,65 @@ const OurSpecialty = () => {
         }
     };
 
+    if (!isMounted) {
+        return null;
+    }
+
     return (
-        <section className={styles.wrapper}>
-            <div className={styles.container}>
-                <h2 className={styles.abilities__up}>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 0.5, duration: 1}} className={styles.big}>Webdev</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1.5, duration: 1}} className={styles.hight}>Optimization</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>Landing</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1, duration: 1}} className={styles.semibold}>SEO</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1.5, duration: 1}} className={styles.hight}>Business</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>Logotype</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1.5, duration: 1}} className={styles.hight}>Social</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>3D Visualization</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>Animation</motion.span>
-                </h2>
-                <h2 className={styles.abilities__down}>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>NextJS</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>TypeScript</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1, duration: 1}} className={styles.semibold}>Website</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>E-commerce</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1.5, duration: 1}} className={styles.hight}>Individual</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>Advertizing</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1, duration: 1}} className={styles.semibold}>Branding</motion.span>
-                    <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 0.5, duration: 1}} className={styles.big}>Stylization</motion.span>
-                </h2>
-            </div>
-        </section>
+        <>
+            {!isMobile ? (
+                <section className={styles.wrapper}>
+                    <div className={styles.container}>
+                        <h2 className={styles.abilities__up}>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 0.5, duration: 1}} className={styles.big}>Webdev</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1.5, duration: 1}} className={styles.hight}>Optimization</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>Landing</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1, duration: 1}} className={styles.semibold}>SEO</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1.5, duration: 1}} className={styles.hight}>Business</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>Logotype</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1.5, duration: 1}} className={styles.hight}>Social</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>3D Visualization</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>Animation</motion.span>
+                        </h2>
+                        <h2 className={styles.abilities__down}>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>NextJS</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>TypeScript</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1, duration: 1}} className={styles.semibold}>Website</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>E-commerce</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1.5, duration: 1}} className={styles.hight}>Individual</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 2, duration: 1}} className={styles.regular}>Advertizing</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 1, duration: 1}} className={styles.semibold}>Branding</motion.span>
+                            <motion.span variants={emergenceVariant} initial="hidden" animate={hasAnimated ? "visible" : "hidden"} onViewportEnter={handleViewportEnter} transition={{delay: 0.5, duration: 1}} className={styles.big}>Stylization</motion.span>
+                        </h2>
+                    </div>
+                </section>
+            ): (
+                <section className={styles.mobile__wrapper}>
+                    <div className={styles.mobile__container}>
+                        <AnimatePresence>
+                            <motion.h2 
+                                className={styles.line__up}
+                                ref={refFirst}
+                                style={{ x: xTranslationFirst }}
+                            >
+                                {[...firstList, ...firstList].map((item) => (
+                                    <span className={styles.item} key={item}>{item}</span>
+                                ))}
+                            </motion.h2>
+                            <motion.h2 
+                                className={styles.line__down}
+                                ref={refSecond}
+                                style={{ x: xTranslationSecond }}
+                            >
+                                {[...secondList, ...secondList].map((item) => (
+                                    <span className={styles.item} key={item}>{item}</span>
+                                ))}
+                            </motion.h2>
+                        </AnimatePresence>
+                    </div>
+                </section>
+            )}
+        </>
     )
 }
 
