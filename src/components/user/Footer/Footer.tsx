ebe662268ae9d0111b7d734c4c631c4c498ea77f
VRomanenko04 <vovasan2004@gmail.com>
2024-10-19 14:00:19 +0300

    Defined the font for the site footer

--- src/components/user/Footer/Footer.tsx
+++ src/components/user/Footer/Footer.tsx
@@ -3,14 +3,17 @@ import Image from 'next/image';
 import styles from './Footer.module.scss';
 import Logo from '@/assets/dispersion_logo.svg';
 import Link from 'next/link';
+import { Be_Vietnam_Pro } from 'next/font/google';
 
 type FooterProps = {
     color: string
 }
 
+const beVietnamPro = Be_Vietnam_Pro({subsets: ["latin"], weight: ["400", "500", "600", "700"] });
+
 const Footer = ({ color }: FooterProps) => {
     return (
-        <footer className={`${styles.footer} ${color === 'blue' ? styles.blue : color === 'magenta' ? styles.magenta : color === 'purple' ? styles.purple : ''}`}>
+        <footer className={`${styles.footer} ${color === 'blue' ? styles.blue : color === 'magenta' ? styles.magenta : color === 'purple' ? styles.purple : ''} ${beVietnamPro.className}`}>
             <div className={styles.footer__container}>
                 <address className={styles.adress}>
                     <h6>Support:</h6>
